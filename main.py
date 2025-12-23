014b1579de03b3df1ee8087ac724c08f49ac9629
umarpathanist <umarpathanist@gmail.com>
2025-12-23 12:01:15 +0530

    Added weather API

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ app.secret_key = "super-secret-key"   # needed for session
 
 # Questions format:
 # [question, option1, option2, option3, option4, correct_option_index]
-questions = [
-    ["Who is Shah Rukh Khan?", "WWE Wrestler", "Plumber", "Actor", "Astronaut", 3],
+
+''' questions = [
+    Who is Shah Rukh Khan?", "WWE Wrestler", "Plumber", "Actor", "Astronaut", 3],
     ["What is the capital of France?", "Berlin", "Paris", "Rome", "London", 2],
     ["Which planet is known as the Red Planet?", "Earth", "Venus", "Mars", "Jupiter", 3],
     ["What is the largest mammal?", "Shark", "Blue Whale", "Elephant", "Giraffe", 2],
@@ -19,9 +20,35 @@ questions = [
     ["What is the fastest land animal?", "Horse", "Lion", "Cheetah", "Elephant", 3],
     ["Which ocean is the largest?", "Indian Ocean", "Pacific Ocean", "Atlantic Ocean", "Arctic Ocean", 2],
     ["What is the smallest country in the world?", "San Marino", "Vatican City", "Monaco", "Liechtenstein", 2]
-]
+] '''
 
-prizes = [500,1500,2500,3500,4500,5500,6500,7500,8500,9500,10500]
+def get_questions():
+     ''' 
+        ye get_questions function se question aa rehe hai.
+    '''
+     return load_questions()
+   
+# Earlier: questions were written directly in Python
+# Now: questions are read dynamically from JSON
+# This makes the quiz customizable and scalable
+
+
+QUESTIONS_FILE = os.path.join(app.root_path, "questions.json")
+
+def load_questions():
+    """Load quiz questions from JSON file"""
+    if not os.path.exists(QUESTIONS_FILE):
+        raise FileNotFoundError("questions.json not found")
+
+    with open(QUESTIONS_FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+# prizes = [500,1500,2500,3500,4500,5500,6500,7500,8500,9500,10500]
+# Prize for each correct answer
+
+prizes_for_corrent = 500
+
 
 # Path to JSON file to store results
 RESULTS_FILE = os.path.join(app.root_path, "results.json")
@@ -57,7 +84,7 @@ def save_result(name, amount, status, correct, wrong):
 #         student_name = request.form.get("name", "").strip()
 
 #         # ✅ Server-side validation
-#         if not student_name.replace(" ", "").isalpha():
+#         i["f not student_name.replace(" ", "").isalpha():
 #             return render_template(
 #                 "index.html",
 #                 error="Name must contain only letters and spaces."
@@ -77,16 +104,20 @@ def save_result(name, amount, status, correct, wrong):
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        name = request.form.get("name", "").strip()
+        name = request.form.get("name", " ").strip()
 
         # Allow only letters
-        if not name.isalpha():
-            return render_template(
-                "index.html",
-                error="Name must contain only letters."
-            )
+        name = name.strip()  # remove extra spaces at start & end
 
-        # 🔥 Character-based rule (a=1, aa=2, aaa=3)
+        
+        if not name.replace(" ", "").isalpha():
+            return render_template(
+        "index.html",
+        error="Name must contain only letters and spaces."
+    )
+
+
+        # Character-based rule (a=1, aa=2, aaa=3)
         if len(name) < 3:
             return render_template(
                 "index.html",
@@ -98,7 +129,108 @@ def home():
 
     return render_template("index.html")
 
+# @app.route("/quiz", methods=["GET", "POST"])
+# def quiz():
+#     if "student_name" not in session:
+#         return redirect(url_for("home"))
 
+#     student_name = session["student_name"]
+
+#     # ✅ SAFETY INITIALIZATION
+#     if "answer_log" not in session:
+#         session["answer_log"] = []
+
+#     if "correct_count" not in session:
+#         session["correct_count"] = 0
+
+#     if "wrong_count" not in session:
+#         session["wrong_count"] = 0
+
+
+#     # initialize defaults
+#     feedback = None
+#     correct_option = None
+#     show_next = False
+
+#     if request.method == "GET":
+#         q_index = 0
+#         current_prize = 0
+#         session["correct_count"] = 0
+#         session["wrong_count"] = 0
+#         session["answer_log"] = []
+
+#     else:
+#         q_index = int(request.form["q_index"])
+#         current_prize = int(request.form["current_prize"])
+
+#         # 🔹 If this POST is for checking answer
+#         if "answer" in request.form:
+#             selected = int(request.form["answer"])
+#             correct = questions[q_index][5]
+
+#             if selected == correct:
+#                 feedback = "correct"
+#                 current_prize += prizes[q_index]
+#                 session["correct_count"] += 1
+#             else:
+#                 feedback = "wrong"
+#                 correct_option = questions[q_index][correct]
+#                 session["wrong_count"] += 1
+
+#                 # ✅ Save question review info (ADD HERE)
+#             session["answer_log"].append({
+#                 "question": questions[q_index][0],
+#                 "options": questions[q_index][1:5],
+#                 "correct_answer": questions[q_index][questions[q_index][5]],
+#                 "user_answer": questions[q_index][selected],
+#                 "is_correct": selected == correct
+#             })
+
+
+
+#             show_next = True   # show Next button
+#             # ❗ DO NOT increment q_index here
+
+#         # 🔹 If this POST is for next question
+#         else:
+#             q_index += 1
+
+#             if q_index >= len(questions):
+#                 save_result(
+#                     student_name,
+#                     current_prize,
+#                     "finished",
+#                     session["correct_count"],
+#                     session["wrong_count"],
+#                     session["answer_log"]
+#                 )
+#                 session["last_attempt_idx"] = len(load_results()) -1
+
+#                 return render_template(
+#                     "result.html",
+#                     name=student_name,
+#                     message="Quiz Completed 🎉",
+#                     amount=current_prize,
+#                     results_count=len(load_results())
+#                 )
+
+#     question = questions[q_index]
+
+#     return render_template(
+#         "quiz.html",
+#         name=student_name,
+#         q_index=q_index,
+#         current_prize=current_prize,
+#         question_text=question[0],
+#         option_a=question[1],
+#         option_b=question[2],
+#         option_c=question[3],
+#         option_d=question[4],
+#         feedback=feedback,
+#         correct_option=correct_option,
+#         show_next=show_next,
+#         questions = questions
+#     )
 
 @app.route("/quiz", methods=["GET", "POST"])
 def quiz():
@@ -107,18 +239,18 @@ def quiz():
 
     student_name = session["student_name"]
 
-    # ✅ SAFETY INITIALIZATION
-    if "answer_log" not in session:
-        session["answer_log"] = []
+    # Load questions from JSON
+    questions = get_questions()
 
-    if "correct_count" not in session:
-        session["correct_count"] = 0
+    # Safety initialization
+    # Initialize Tracking Variables
+    session.setdefault("answer_log", [])
+    # stores every question + answer history.
+    session.setdefault("correct_count", 0)
+    # right answers count
+    session.setdefault("wrong_count", 0)
+    # wrong answers count 
 
-    if "wrong_count" not in session:
-        session["wrong_count"] = 0
-
-
-    # initialize defaults
     feedback = None
     correct_option = None
     show_next = False
@@ -126,43 +258,51 @@ def quiz():
     if request.method == "GET":
         q_index = 0
         current_prize = 0
+        session["answer_log"] = []
         session["correct_count"] = 0
         session["wrong_count"] = 0
-        session["answer_log"] = []
 
     else:
         q_index = int(request.form["q_index"])
         current_prize = int(request.form["current_prize"])
 
-        # 🔹 If this POST is for checking answer
+        # 🔹 Answer submission
         if "answer" in request.form:
             selected = int(request.form["answer"])
-            correct = questions[q_index][5]
 
+            # Extract data from JSON structure
+            # Get current question as dictionary
+            question = questions[q_index]
+            # current question
+            options = question["options"]
+            # uske options
+            correct = question["correct_option"]
+            # correct option number
+
+            # Compare user answer with correct option from JSON
             if selected == correct:
                 feedback = "correct"
-                current_prize += prizes[q_index]
+                # Add fixed prize for correct answer
+                current_prize += prizes_for_corrent
                 session["correct_count"] += 1
             else:
                 feedback = "wrong"
-                correct_option = questions[q_index][correct]
+                # correct_option = options[correct - 1]
+                current_prize -= 250 
                 session["wrong_count"] += 1
 
-                # ✅ Save question review info (ADD HERE)
+            # Save each question attempt for review page
             session["answer_log"].append({
-                "question": questions[q_index][0],
-                "options": questions[q_index][1:5],
-                "correct_answer": questions[q_index][questions[q_index][5]],
-                "user_answer": questions[q_index][selected],
+                "question": question["question"],
+                "options": options,
+                "correct_answer": options[correct - 1],
+                "user_answer": options[selected - 1],
                 "is_correct": selected == correct
             })
 
+            show_next = True
 
-
-            show_next = True   # show Next button
-            # ❗ DO NOT increment q_index here
-
-        # 🔹 If this POST is for next question
+        #  Next question
         else:
             q_index += 1
 
@@ -175,33 +315,51 @@ def quiz():
                     session["wrong_count"],
                     session["answer_log"]
                 )
-                session["last_attempt_idx"] = len(load_results()) -1
+                session["last_attempt_idx"] = len(load_results()) - 1
 
+             # CALCULATE ACCURACY HERE        
+                total_questions = len(questions)
+                correct_answers = session["correct_count"]
+                percentage = (correct_answers / total_questions) * 100
+                is_winner = percentage >= 70
+
+                # return render_template(
+                #     "result.html",
+                #     name=student_name,
+                #     message="Quiz Completed 🎉",
+                #     amount=current_prize,
+                #     results_count=len(load_results())
+                # )
+
+                # User ko final result dikhaya.
                 return render_template(
-                    "result.html",
-                    name=student_name,
-                    message="Quiz Completed 🎉",
-                    amount=current_prize,
-                    results_count=len(load_results())
-                )
+                "result.html",
+                name=student_name,
+                message="Quiz Completed 🎉",
+                amount=current_prize,
+                percentage=round(percentage, 2),
+                is_winner=is_winner,
+                # results_count=(load_results())
+            )
 
+
+    # Current question extract:
     question = questions[q_index]
 
+    # Frontend ko data bheja.
     return render_template(
         "quiz.html",
         name=student_name,
         q_index=q_index,
         current_prize=current_prize,
-        question_text=question[0],
-        option_a=question[1],
-        option_b=question[2],
-        option_c=question[3],
-        option_d=question[4],
+        question_text=question["question"], #
+        options=question["options"], #
         feedback=feedback,
         correct_option=correct_option,
         show_next=show_next,
-        questions = questions
+        questions=questions
     )
+
 
 def save_result(name, amount, status, correct, wrong, answers):
     results = load_results()
@@ -241,7 +399,8 @@ def leaderboard():
 def review(idx):
     results = load_results()
 
-    # safety check
+    # Agar index galat hua
+    # Toh wapas leaderboard bhej do
     if idx < 0 or idx >= len(results):
         return redirect(url_for("leaderboard"))
 
@@ -252,8 +411,5 @@ def review(idx):
         attempt=attempt
     )
 
-
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+     app.run(debug=True)
